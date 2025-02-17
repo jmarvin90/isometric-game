193a4dbc579301646943cd3ee57abc6f48afcf32
marv <j_marvin@mail.com>
2025-02-17 13:54:21 +0000

    Fix sprite constructor initialiser list

--- src/engine/components/sprite.h
+++ src/engine/components/sprite.h
@@ -15,12 +15,13 @@ struct Sprite {
     Sprite(
         SDL_Texture* texture,
         SDL_Rect& source_rect
-    ): texture{texture}, source_rect{source_rect} {
-        offset = {
+    ): 
+        texture{texture}, 
+        source_rect{source_rect},
+        offset{
             constants::TILE_SIZE.x - source_rect.w, 
             constants::TILE_SIZE.y - source_rect.h
-        };
-    }
+        } {}
 };
 
 #endif
