b910fa876683660a5d174c888a2af9ab5355a18c
marv <j_marvin@mail.com>
2025-04-09 15:20:56 +0100

    latest working logic

--- prototypes/graph/geometry.py
+++ prototypes/graph/geometry.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+from enum import Enum
+# from graph.constants import directions_dict
+import math
+import functools
 
 class Point:
     def __init__(self, x: int, y: int):
@@ -36,6 +40,36 @@ class Point:
             self.x - comparator.x, self.y - comparator.y
         )  
     
+    @functools.cached_property
+    def vector_direction(self) -> Point:
+        return Point(
+            int(math.copysign(1, self.x)) if self.x != 0 else 0,
+            int(math.copysign(1, self.y)) if self.y != 0 else 0
+        )
+    
+    @functools.cached_property
+    def vector_direction_bitmask(self) -> int:
+        for direction, vector in directions_dict.items():
+            if vector == self.vector_direction:
+                return direction
+
+
     def is_adjacent_to(self, comparator: Point):
         diff = abs(self - comparator)
         return diff.x <= 1 and diff.y <= 1 and diff != Point(0, 0)
+
+    
+class Directions(Enum):
+    NORTH = 8
+    EAST = 4
+    SOUTH = 2
+    WEST = 1
+    NO_DIRECTION = 0
+
+directions_dict = {
+    Directions.NORTH.value: Point(0, -1),
+    Directions.EAST.value: Point(1, 0),
+    Directions.SOUTH.value: Point(0, 1),
+    Directions.WEST.value: Point(-1, 0),
+    Directions.NO_DIRECTION.value: Point(0, 0)
+}
--- prototypes/graph/tilemap.py
+++ prototypes/graph/tilemap.py
@@ -1,33 +1,109 @@
 from __future__ import annotations
-import typing
-from graph.geometry import Point
-from graph.constants import Directions, directions_dict
+from graph.geometry import Point, Directions, directions_dict
+# from graph.constants import Directions, directions_dict
 from graph.utils import count_set_bits
+
 
 class Tile:
     """Represents a tile on the TileMap."""
-    def __init__(self, position: Point):
-        self.position = position        # the position on the grid
-        self.connections = 0            # the connections bitmask - NESW
+    def __init__(self, position: Point, tilemap: TileMap):
+        self.position = position                # the position on the grid
+        self.__tilemap = tilemap
+
+        self.tile_connection_bitmask = 0        # the connections bitmask - NESW
+        self.connected_tiles = set()            # Node connections (edges)
+
+    def __scan(self, direction: int) -> Tile:
+        """Return the the most distant tile connected in the given direction."""
+        opposing_direction = ((direction >> 2 | direction << 2) & 15)
+
+        valid = True
+        current_tile = self
+
+        while valid:
+
+            next_tile = self.__tilemap[
+                current_tile.position + directions_dict[direction]
+            ]
+
+            # Check if the next tile has a connection further on in the same 
+            # direction; remove the connection and return the termination point
+            # if so
+            for connected_tile in next_tile.connected_tiles:
+                if (
+                    connected_tile.position - next_tile.position
+                ).vector_direction_bitmask == direction:
+                    next_tile.__disconnect(connected_tile)
+                    connected_tile.__disconnect(next_tile)
+                    return connected_tile
+
+            valid = next_tile.tile_connection_bitmask & opposing_direction
+
+            if valid:
+                current_tile = next_tile
+
+        return current_tile
+    
+    def __connect(self, tile: Tile) -> None:
+        self.connected_tiles.add(tile)
+        self.__tilemap.nodes = self.__tilemap.nodes | {tile, self}
+
+    def __disconnect(self, tile: Tile) -> None:
+        self.connected_tiles.remove(tile)
+        self.__tilemap.nodes = self.__tilemap.nodes - {tile, self}
 
     def connect(self, directions: int) -> None:
         """Update the tile's connections to connect in specified directions."""
-        self.connections = self.connections | directions
+        disconnected_directions = self.tile_connection_bitmask & ~directions
+        self.tile_connection_bitmask = self.tile_connection_bitmask | directions
+
+        output = {}
+        for direction in (1, 2, 4, 8):
+            if directions & direction:
+                output[direction] = self.__scan(direction)
+            else:
+                output[direction] = self
+
+        # Create a node from the current tile if it connects in more than two 
+        # directions, or the connections represent a direction change
+        if (
+            count_set_bits(directions) != 2 
+            or directions not in (
+                Directions.EAST.value | Directions.WEST.value, 
+                Directions.NORTH.value | Directions.SOUTH.value
+            )
+        ):
+            for connected_tile in output.values():
+                if connected_tile.position != self.position:
+                    self.__connect(connected_tile)
+                    connected_tile.__connect(self)
+            return
+        
+        for lhs, rhs in (
+            (output[Directions.EAST.value], output[Directions.WEST.value]),
+            (output[Directions.NORTH.value], output[Directions.SOUTH.value])
+        ):
+            if lhs != rhs:
+                lhs.__connect(rhs)
+                rhs.__connect(lhs)
 
     def clear_connections(self) -> None:
         """Clear the tile's connections."""
-        self.connections = 0
+        self.tile_connection_bitmask = 0
+        self.connected_tiles = set()
 
 
 class TileMap:
     """Represents a (square) tilemap as a list of tiles."""
     def __init__(self, size: int):
         self.size = size
-        self.graph = Graph(self)
+
         self.tiles = [
-            Tile(Point(number % self.size, number // self.size)) 
+            Tile(Point(number % self.size, number // self.size), self) 
             for number in range(0, self.size**2)
         ]
+
+        self.nodes = set()
 
     def __getitem__(self, position: Point) -> Tile:
         """Get the tile from a point (x,y) position on the tilemap."""
@@ -38,164 +114,4 @@ class TileMap:
     def clear_connections(self) -> None:
         """Clear connections for every tile, and reset the graph."""
         for tile in self.tiles:
-            tile.clear_connections
-        self.graph.reset()      
-    
-    # TODO: think of a better name
-    def connect(self, position: Point, directions: int) -> None:
-        """Connect a tile at a given point/in specified directions."""
-        # Helpful to do this at the TileMap level to enable the graph logic
-        tile = self[position]
-        tile.connect(directions)
-        self.graph.add_node(tile)
-
-
-class Edge:
-    """Represents an edge on the graph, between two Tiles."""
-    def __init__(self, origin: Tile, termination: Tile, tilemap: TileMap):
-        self.origin = origin
-        self.termination = termination
-        self.tilemap = tilemap
-
-    def __str__(self) -> str:
-        """Get a string representation of the edge as (start)->(end)."""
-        return f"{self.origin.position} -> {self.termination.position}"
-    
-    @property
-    def is_horizontal(self) -> bool:
-        """Whether the edge is horizontal."""
-        return self.origin.position.y == self.termination.position.y
-    
-    @property
-    def is_vertical(self) -> bool:
-        """Whether the edge is vertical."""
-        return self.origin.position.x == self.termination.position.x
-
-    @property
-    def direction_bitmask(self) -> int:
-        if self.is_horizontal:
-            return 5
-
-        if self.is_vertical:
-            return 10
-    
-    def covers(self, point: Point) -> bool:
-        """Whether the edge 'covers' a given point."""
-        if self.is_horizontal:
-            smallest, largest = sorted(
-                (self.origin.position.x, self.termination.position.x)
-            )
-            return smallest <= point.x <= largest
-
-        if self.is_vertical:
-            smallest, largest = sorted(
-                (self.origin.position.y, self.termination.position.y)
-            )
-            return smallest <= point.y <= largest
-        
-        return False
-
-    def __crossing_point(self, comparator: Edge) -> Point:
-        """Get the theoretical crossing point with the comparator."""
-        vert = self if self.is_vertical else comparator
-        horiz = self if self.is_horizontal else comparator
-        
-        # Get the theoretical crossing point
-        return Point(
-            vert.origin.position.x,
-            horiz.origin.position.y
-        )
-    
-    def __intersects(self, comparator: Edge) -> bool:
-        """Check if the edge crossed the comparator."""
-        # Assume not, if the two lines are colinear 
-        if (
-            self.is_vertical == comparator.is_vertical and
-            self.is_horizontal == comparator.is_horizontal
-        ):
-            return False
-
-        crossing_point = self.__crossing_point(comparator)
-
-        # Lines cross if they both cover the theoretical crossing point
-        return (
-            self.covers(crossing_point) and 
-            comparator.covers(crossing_point)
-        )
-
-
-class Graph:
-    """Represent connections between Tiles on the TileMap."""
-    def __init__(self, tilemap: TileMap):
-        self.reset()
-
-        self.__tilemap = tilemap
-
-    def reset(self) -> None:
-        """Reset the graph by removing all nodes and edges."""
-        self.connected_nodes = set()
-        self.orphan_nodes = set()
-        self.edges = set()
-
-    def add_edge(self, new_edge: Edge) -> None:
-        self.edges.add(new_edge)
-
-    def add_node(self, tile: Tile) -> None:
-        """Logic to add a node to the graph - adding edges where needed."""
-        # All the directions the new node connects in
-        directions = (
-            direction for direction in (1, 2, 4, 8)
-            if direction & tile.connections
-        )
-
-        # The new edges we're going to create
-        edges = set()
-
-        # Go in every direction
-        for direction in directions:
-
-            valid = True
-
-            opposing_direction = (
-                (direction >> 2 | direction << 2) & 15
-            )
-
-            vector = directions_dict[direction]
-            current_tile = tile
-
-            while valid:
-                # Get the next tile in the same direction
-                next_tile = self.__tilemap[current_tile.position + vector]
-
-                # If that tile reciprocates the connection, keep going
-                if next_tile.connections & opposing_direction:
-                    current_tile = next_tile
-                # Otherwise, stop here
-                else:
-                    valid = False
-
-            # If we started and ended on a different tile
-            if current_tile != tile:
-                # Then create a new edge from start -> finish
-                edges.add(Edge(tile, current_tile, self.__tilemap))
-
-        self.edges = self.edges | edges
-
-    def remove_node(self, tile: Tile) -> None:
-        """Remove a node from the graph, including any related edges."""
-        # exit out if the tile isn't in the graph
-        if not tile in self.nodes:
-            return
-        
-        # remove the tile as a node
-        self.nodes.remove(tile)
-        
-        # get any edges that the tile was part of
-        affected_edges = {
-            edge for edge in self.edges 
-            if edge.origin == tile
-            or edge.termination == tile
-        }
-
-        # remove any edges it was part of, and remove it from the list of nodes
-        self.edges = self.edges - affected_edges
+            tile.clear_connections()
