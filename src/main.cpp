1a59625c50b5831e717c8e60e4560fc4cb668b5a
marv <j_marvin@mail.com>
2025-05-07 19:43:13 +0100

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,77 +4,65 @@
 
 #include "tilemap.h"
 
-int main() {
+int main()
+{
     Game game;
 
     game.initialise();
 
-    TileMap* tilemap {game.get_tilemap()};
-    
-    (*tilemap)[{8, 0}].add_building_level(
-        game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+    TileMap *tilemap{game.get_tilemap()};
 
     (*tilemap)[{8, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{8, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{8, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{8, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
+
+    (*tilemap)[{8, 0}].add_building_level(
+        game.building_tiles->get_spritesheet_texture(),
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{7, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{7, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{7, 0}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_043.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_043.png"));
 
     (*tilemap)[{6, 2}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_053.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_053.png"));
 
     (*tilemap)[{6, 2}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_053.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_053.png"));
 
     (*tilemap)[{6, 2}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_053.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_053.png"));
 
     (*tilemap)[{6, 2}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_053.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_053.png"));
 
     (*tilemap)[{6, 2}].add_building_level(
         game.building_tiles->get_spritesheet_texture(),
-        game.building_tiles->get_sprite_rect("buildingTiles_061.png")
-    );
+        game.building_tiles->get_sprite_rect("buildingTiles_061.png"));
 
     game.run();
     game.destroy();
