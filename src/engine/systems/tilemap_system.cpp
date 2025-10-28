6c9ee3a38df2128f9e2b5ab9c0b5e89470ab358f
marv <j_marvin@mail.com>
2025-10-28 16:44:59 +0000

    Tile highlighting

--- src/engine/systems/tilemap_system.cpp
+++ src/engine/systems/tilemap_system.cpp
@@ -8,32 +8,40 @@ void apply_highlight(entt::registry& registry, const Tile* tile, int factor=1) {
     }
 }
 
-void TileMapSystem::update(
-    entt::registry& registry,
-    const bool debug_mode
-) {
+void TileMapSystem::update(entt::registry& registry, const bool debug_mode) {
+    auto& tilemap = registry.ctx().get<TileMapComponent>();
+    const auto& mouse = registry.ctx().get<const MouseComponent>();
+
     if (!debug_mode) {
+        if (tilemap.highlighted_tile) {
+            apply_highlight(registry, tilemap.highlighted_tile, -1);
+            tilemap.highlighted_tile = nullptr;
+        }
         return;
     }
 
-    TileMapComponent& tilemap {registry.ctx().get<TileMapComponent>()};
-    const MouseComponent& mouse {registry.ctx().get<const MouseComponent>()};
-    
-    GridPosition grid_position {
-        ScreenPosition(mouse.window_current_position).to_grid_position(registry)
+    if (!mouse.moved) {
+        return;
+    }
+
+    const GridPosition grid_position {
+        ScreenPosition(mouse.window_current_position)
+        .to_grid_position(registry)
     };
 
-    if (mouse.moved) {
-        if (tilemap.highlighted_tile) {
-            apply_highlight(registry, tilemap.highlighted_tile, -1);
-        }
+    Tile* new_tile = tilemap[grid_position];
 
-        tilemap.highlighted_tile = nullptr;
-        Tile* highlighted_tile {tilemap[grid_position]};
-
-        if (highlighted_tile) {
-            apply_highlight(registry, highlighted_tile);
-            tilemap.highlighted_tile = highlighted_tile;
-        }
+    if (new_tile == tilemap.highlighted_tile) {
+        return;
     }
+
+    if (tilemap.highlighted_tile) {
+        apply_highlight(registry, tilemap.highlighted_tile, -1);
+    }
+
+    if (new_tile) {
+        apply_highlight(registry, new_tile);
+    }
+
+    tilemap.highlighted_tile = new_tile;
 }
