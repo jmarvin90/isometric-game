2975fa747dbd06f4a46787c043d69ea8738085e3
marv <j_marvin@mail.com>
2025-11-11 11:45:43 +0000

    better comments

--- src/engine/directions.h
+++ src/engine/directions.h
@@ -14,14 +14,14 @@
 
 namespace Direction {
     enum class TDirection : uint8_t {
-        NORTH = 1 << 0, // 64
-        NORTH_WEST = 1 << 1, // 128
-        WEST = 1 << 2, // 1
-        SOUTH_WEST = 1 << 3, // 2
-        SOUTH = 1 << 4, // 4
-        SOUTH_EAST = 1 << 5, // 8
-        EAST = 1 << 6, // 16
-        NORTH_EAST = 1 << 7, // 32
+        NORTH = 1 << 0,
+        NORTH_WEST = 1 << 1,
+        WEST = 1 << 2,
+        SOUTH_WEST = 1 << 3,
+        SOUTH = 1 << 4,
+        SOUTH_EAST = 1 << 5,
+        EAST = 1 << 6,
+        NORTH_EAST = 1 << 7
     };
 
     template <typename T>
