ffc0451b78106d7132284f6105b7ac59e611d522
marv <j_marvin@mail.com>
2025-12-19 11:19:24 +0000

    Constant scene border

--- src/engine/constants.h
+++ src/engine/constants.h
@@ -9,6 +9,7 @@
 namespace constants {
     inline constexpr int FPS { 60 };
     inline constexpr float MILLIS_PER_FRAME { 1'000.f / FPS };
+    inline constexpr glm::ivec2 scene_border_px { 150, 150 };
     const std::string spritesheet { "assets/spritesheet_scaled.png" };
 } // namespace constants
 
