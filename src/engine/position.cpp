46b2befd8c27266f4b37f6730b56a817c5c6f6a9
marv <j_marvin@mail.com>
2025-12-19 10:03:42 +0000

    formatting

--- src/engine/position.cpp
+++ src/engine/position.cpp
@@ -2,6 +2,7 @@
 #include <components/camera_component.h>
 #include <components/tilespec_component.h>
 #include <components/transform_component.h>
+#include <constants.h>
 #include <position.h>
 #include <spdlog/spdlog.h>
 
@@ -9,10 +10,7 @@ const WorldPosition ScreenPosition::to_world_position(
     const entt::registry& registry) const
 {
     const CameraComponent& camera { registry.ctx().get<const CameraComponent>() };
-
-    // TODO: Replace with scene description
-    const glm::ivec2 scene_border_px { 150, 150 };
-    return WorldPosition { (position + camera.position) - scene_border_px };
+    return WorldPosition { (position + camera.position) - constants::scene_border_px };
 }
 
 const GridPosition ScreenPosition::to_grid_position(
@@ -25,17 +23,13 @@ const ScreenPosition WorldPosition::to_screen_position(
     const entt::registry& registry) const
 {
     const CameraComponent& camera { registry.ctx().get<const CameraComponent>() };
-
-    // TODO: Replace with scene description
-    const glm::ivec2 scene_border_px { 150, 150 };
-    return ScreenPosition { (position - camera.position) + scene_border_px };
+    return ScreenPosition { (position - camera.position) + constants::scene_border_px };
 }
 
 const glm::vec2 WorldPosition::to_grid_gross(const entt::registry& registry) const
 {
     const TileSpecComponent& tile_spec { registry.ctx().get<TileSpecComponent>() };
     const TileMapComponent& tilemap { registry.ctx().get<TileMapComponent>() };
-
     const glm::ivec2 world_pos_adjusted { position - tile_spec.centre };
     const glm::ivec2 centred_world_pos { world_pos_adjusted - tilemap.origin_px };
     return glm::vec2 { tile_spec.matrix_inverted * centred_world_pos };
@@ -52,7 +46,6 @@ const GridPosition WorldPosition::to_grid_position(
 GridPosition::GridPosition(const entt::registry& registry, const int tile_n)
 {
     const TileMapComponent& tilemap { registry.ctx().get<TileMapComponent>() };
-
     if (tile_n < tilemap.tiles_per_row) {
         position = { tile_n, 0 };
     } else {
@@ -73,11 +66,8 @@ const WorldPosition GridPosition::to_world_position(
         registry.ctx().get<const TileSpecComponent>()
     };
     const TileMapComponent& tilemap { registry.ctx().get<const TileMapComponent>() };
-
     const glm::ivec2 movement { tilespec.matrix * position };
-
     const glm::ivec2 world_pos_gross { (movement + tilemap.origin_px) - tilespec.centre };
-
     return WorldPosition { world_pos_gross + tilespec.centre };
 }
 
