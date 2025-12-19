8d34d76fa059b0e1c185e241e20271f4aa44966a
marv <j_marvin@mail.com>
2025-12-19 11:26:20 +0000

    Camera scroll speed constant

--- src/engine/constants.h
+++ src/engine/constants.h
@@ -7,9 +7,10 @@
 #include <string>
 
 namespace constants {
+    inline constexpr glm::ivec2 CAMERA_SCROLL_SPEED { 10, 10 };
     inline constexpr int FPS { 60 };
     inline constexpr float MILLIS_PER_FRAME { 1'000.f / FPS };
-    inline constexpr glm::ivec2 scene_border_px { 150, 150 };
+    inline constexpr glm::ivec2 SCENE_BORDER_PX { 150, 150 };
     const std::string spritesheet { "assets/spritesheet_scaled.png" };
 } // namespace constants
 
--- src/engine/systems/camera_system.cpp
+++ src/engine/systems/camera_system.cpp
@@ -1,6 +1,7 @@
 #include <components/camera_component.h>
 #include <components/mouse_component.h>
 #include <components/tilemap_component.h>
+#include <constants.h>
 #include <systems/camera_system.h>
 
 void CameraSystem::update(entt::registry& registry,
@@ -10,21 +11,21 @@ void CameraSystem::update(entt::registry& registry,
     CameraComponent& camera { registry.ctx().get<CameraComponent>() };
     const TileMapComponent& tilemap { registry.ctx().get<const TileMapComponent>() };
 
-    const glm::ivec2 screen_border_px { 150, 150 };
-
-    if (mouse.window_current_position.x < screen_border_px.x && camera.position.x > 0) {
-        camera.position.x -= 10;
+    if (mouse.window_current_position.x < constants::SCENE_BORDER_PX.x && camera.position.x > 0) {
+        camera.position.x -= constants::CAMERA_SCROLL_SPEED.x;
     }
 
-    if (mouse.window_current_position.y < screen_border_px.y && camera.position.y > 0) {
-        camera.position.y -= 10;
+    if (mouse.window_current_position.y < constants::SCENE_BORDER_PX.y && camera.position.y > 0) {
+        camera.position.y -= constants::CAMERA_SCROLL_SPEED.y;
     }
 
-    if ((display_mode.w - mouse.window_current_position.x) < screen_border_px.x && (camera.position.x + display_mode.w) < (tilemap.area.x + (screen_border_px.x * 2))) {
-        camera.position.x += 10;
+    if (
+        (display_mode.w - mouse.window_current_position.x) < constants::SCENE_BORDER_PX.x && (camera.position.x + display_mode.w) < (tilemap.area.x + (constants::SCENE_BORDER_PX.x * 2))) {
+        camera.position.x += constants::CAMERA_SCROLL_SPEED.x;
     }
 
-    if ((display_mode.h - mouse.window_current_position.y) < screen_border_px.y && (camera.position.y + display_mode.h) < (tilemap.area.y + (screen_border_px.y * 2))) {
-        camera.position.y += 10;
+    if (
+        (display_mode.h - mouse.window_current_position.y) < constants::SCENE_BORDER_PX.y && (camera.position.y + display_mode.h) < (tilemap.area.y + (constants::SCENE_BORDER_PX.y * 2))) {
+        camera.position.y += constants::CAMERA_SCROLL_SPEED.y;
     }
 }
