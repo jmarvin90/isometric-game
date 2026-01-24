5b84cb7942f3753ff8dc9048d29674dff7c4467f
marv <j_marvin@mail.com>
2026-01-24 18:52:38 +0000

    Fixed duplicate check

--- src/engine/components/spatialmap_component.cpp
+++ src/engine/components/spatialmap_component.cpp
@@ -15,14 +15,9 @@ entt::entity SpatialMapComponent::operator[](const int cell_number) const
 entt::entity SpatialMapComponent::operator[](const glm::ivec2 grid_position) const
 {
     SpatialMapGridPosition grid_pos { grid_position };
-
-    // TODO: why is it necessary to convert to a world position here
     int cell_number { Position::to_spatial_map_cell(grid_pos, *this) };
 
-    if (
-        !Position::is_valid(grid_pos, *this) || //
-        map.find(cell_number) == map.end()
-    ) {
+    if (!Position::is_valid(grid_pos, *this)) {
         return entt::null;
     }
 
